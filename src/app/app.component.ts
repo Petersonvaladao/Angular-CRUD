81af0712ead05e55514f6daea3e1964aeec2ea47
Peterson Valadão <73299678+Petersonvaladao@users.noreply.github.com>
2022-11-24 13:14:03 -0300

    CRUD first part (delete)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-ap';
+  title: string = 'my-ap';
+  tasks = [
+    "Visit someone",
+    "Go somewhere",
+    "Travel someday",
+    "Stay somehow",
+    "Shop somewhat?",
+  ]
+
+  add(newTask: string) {
+    this.tasks.push(newTask)
+  }
+
+  remove(existingTask: string) {
+    let userConfirmed = confirm(`Are you sure that you wanto to remove the following task? \n "${existingTask}"`)
+
+    if(userConfirmed) {
+      this.tasks = this.tasks.filter(task => task != existingTask)
+    }
+  }
 }
